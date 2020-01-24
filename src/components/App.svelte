f26b6a40ad385858af450f09f36be9f8b0069daf
Billy Clark <billy_clark@sil.org>
2020-01-24 15:44:27 -0500

    preliminary glyph choice added

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,13 +1,17 @@
 <script>
 import Textfield from '@smui/textfield'
+import Radio from '@smui/radio'
+import FormField from '@smui/form-field'
 
 let name = 'Billy Clark'
 let role = 'GTIS Application Developer'
 let number = '704-491-6396'
 let email = 'billy_clark@sil.org'
+let glyph = '1'
 </script>
 
 <style>
+/* TODO: source sans pro */
 :global(body) {
   margin: 3rem;
 }
@@ -56,11 +60,33 @@ img {
       <Textfield bind:value={email} label="Email" />
     </div>
 
-    
+    <div>
+      Glyphs:
+      <FormField>
+        <Radio bind:group={glyph} value="1" />
+        <span slot="label">1</span>
+      </FormField>
+      <FormField>
+        <Radio bind:group={glyph} value="2" />
+        <span slot="label">2</span>
+      </FormField>
+      <FormField>
+        <Radio bind:group={glyph} value="3" />
+        <span slot="label">3</span>
+      </FormField>
+      <FormField>
+        <Radio bind:group={glyph} value="4" />
+        <span slot="label">4</span>
+      </FormField>
+      <FormField>
+        <Radio bind:group={glyph} value="5" />
+        <span slot="label">5</span>
+      </FormField>
+    </div>
   </div>
 
   <section class="mdc-elevation--z3 row grow p">
-    <img src={`glyph1.png`} alt="logo">
+    <img src={`glyph${glyph}.png`} alt="logo">
 
     <div>
       <div>
@@ -70,7 +96,7 @@ img {
         {role}
       </div>
       <div>
-        {number} | {email}
+        {number} <span>{number && email && '|'}</span> {email}
       </div>
     </div>
   </section>
